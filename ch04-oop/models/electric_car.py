6a08905030787ae9c700988a9a1f97313632be21
Rob Prouse <rob@prouse.org>
2020-01-20 21:59:41 -0500

    Add types to the electric car constructor

--- ch04-oop/models/electric_car.py
+++ ch04-oop/models/electric_car.py
@@ -2,7 +2,7 @@ from models.car import Car
 
 class ElectricCar(Car):
 
-    def __init__(self, model_name, base_price):
+    def __init__(self, model_name: str, base_price: float):
         super().__init__(model_name, "electric", 0, base_price)
 
     def drive(self):
